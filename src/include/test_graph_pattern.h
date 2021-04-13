e84f47b3d550b97bc4acb75a807be8db909c996f
GUERIN Leo <Leo.Guerin@bordeaux-inp.fr>
2021-04-13 15:02:12 +0200

    ajout du point h pour les tests

---
+++ src/include/test_graph_pattern.h
@@ -0,0 +1,18 @@
+#ifndef TEST_GRAPH_PATTERN_H_
+#define TEST_GRAPH_PATTERN_H_
+
+#include "graph_pattern.h"
+
+
+enum direction_t { NO_DIRECTION=0, NORTH, SOUTH, WEST, EAST, MAX_DIRECTION };
+
+
+// get the index of direction of j according to i
+int get_direction_square(size_t i,size_t j, size_t m);
+gsl_spmatrix_uint *fill_graph(size_t m, int (*is_not_connected)(size_t, size_t, size_t));
+int square(size_t x, size_t y, size_t m);
+gsl_spmatrix_uint *square_graph(size_t m);
+int is_in_hole(size_t x, size_t y, size_t begin_x, size_t begin_y, size_t size_x, size_t size_y);
+int toric(size_t x, size_t y, size_t m);
+
+#endif // TEST_GRAPH_PATTERN_H_
