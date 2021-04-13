5afe5bed5ea6ac3fef16e0b09a38bb191c549127
MARAIS Lucas <Lucas.Marais@bordeaux-inp.fr>
2021-04-13 17:07:01 +0200

    player.c

--- src/player/player.c
+++ src/player/player.c
@@ -26,7 +26,7 @@ char const* get_player_name()
 }
 
 /*
- * Initialize the player with informations given by the server :
+ * Initialize the player with informations given by the server if it is not already initialize :
  * - id : the id of the player : WHITE or BLACK
  * - graph : the graph to play with
  * - num_walls : the number of walls in the hand of the player
@@ -65,8 +65,13 @@ struct move_t get_first_move()
   size_t random_index = (size_t) rand() % nb_pos;
   player.position[player.id] = starting_positions[random_index];
   
-  struct move_t first_move = {.m = player.position[player.id], .e = {{-1,-1}},
-			       .t = MOVE, .c = player.id}; 
+  struct move_t first_move;
+  first_move.m = player.position[player.id];
+  first_move.e[0] = no_edge();
+  first_move.e[1] = no_edge();
+  first_move.t = MOVE;
+  first_move.c = player.id;
+  
   return first_move;
 }
 
@@ -74,36 +79,46 @@ struct move_t get_first_move()
  * Update the player with the move of the other player and return a move
  * - previous_move : the previous move of the other player
  */
-struct move_t get_new_move(struct move_t previous_move)
+struct move_t get_new_move()
 {
   size_t nb_pos = 0;
   size_t pos[player.graph->num_vertices];
 
-  size_t position_player1 = player.id == BLACK ? player.position[player.id] : player.position[previous_move.c];
-  size_t position_player2 = player.id == BLACK ? player.position[previous_move.c] : player.position[player.id];
+  size_t position_player1 = player.position[BLACK];
+  size_t position_player2 = player.position[WHITE];
   
-  
+  //research of available positions
   for (size_t j = 0; j < player.graph->num_vertices; j++) {
     if (can_player_move_to(player.graph, j, player.id, position_player1, position_player2))
       pos[nb_pos++] = j;
   }
 
+  //choice of an available position
   size_t ind = (size_t) rand() % nb_pos;
   player.position[player.id] = pos[ind];
 
-  struct move_t new_move = {.m = player.position[player.id], .e = {{-1,-1}},
-			    .t = MOVE, .c = player.id};
+  //move to the position
+  struct move_t new_move;
+  new_move.m = player.position[player.id];
+  new_move.e[0] = no_edge();
+  new_move.e[1] = no_edge();
+  new_move.t = MOVE;
+  new_move.c = player.id;
+  
   return new_move;
 }
 
 /*
- * Return true if the player is not placed yet
+ * Return true if the player is not placed yet.
  */
 int is_first_move()
 {
   return player.position[player.id] == player.graph->num_vertices;
 }
 
+/*
+ * Update the graph of the player with the move of the other player
+ */
 void update(struct move_t previous_move)
 {
   if (previous_move.t == MOVE) {
@@ -123,7 +138,7 @@ struct move_t play(struct move_t previous_move)
 
   update(previous_move);
 
-  return get_new_move(previous_move);
+  return get_new_move();
 }
 
 
