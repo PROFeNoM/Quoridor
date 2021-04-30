fd3d1a695aa25456eda5bd77cae1a62a5eae0aaf
SORNAY Jean-Francois <Jean-Francois.Sornay@bordeaux-inp.fr>
2021-04-30 13:31:24 +0200

    Clean dijsktra algorithm

--- src/common/dijsktra.c
+++ src/common/dijsktra.c
@@ -1,168 +1,92 @@
 #include "utils.h"
 #include "dijsktra.h"
+#include <string.h>
 
-struct dijsktra_t {
+//-------------
+//| 2 | 3 | 4 |
+//-------------
+//| 1 | 2 | 3 |
+//-------------
+//| . | 1 | 2 |
+//-------------
 
-  size_t dist;
-  size_t prec;
-};
+//-------------
+//|   | 5 |   |
+//-------------
+//| 4 | 1 | 6 |
+//-------------
+//| 2 | . | 3 |
+//-------------
+
+//-------------
+//| 3 | 6 |   |
+//-------------
+//| . | 2 | 5 |
+//-------------
+//| 1 | 4 |   |
+//-------------
 
 
-struct dijsktra_t * init_dijsktra(size_t dist, size_t prec){
-
-  struct dijsktra_t *  d = malloc(sizeof(struct dijsktra_t));
-  d->dist = dist;
-  d->prec = prec;
-  return d;
+void init_array_with_value(size_t value, size_t visited[], size_t num)
+{
+    for (size_t i = 0; i < num; i++)
+        visited[i] = value;
 }
 
-void free_dijsktra(struct dijsktra_t * d){
-
-  free(d);
+int all_visited(size_t visited[], size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+        if (!visited[i])
+            return 0;
+    return 1;
 }
 
-  
+int is_visited(size_t visited[], size_t index)
+{
+    return visited[index] != 0;
+}
 
-int all_visited(gsl_spmatrix_uint * visited){
-
-  for (size_t i = 0 ; i < visited->size2 ; i++){
-    if (gsl_spmatrix_uint_get(visited,0,i) == 0){
-      return 0;
+size_t get_dist_min_not_visited(size_t *dist, size_t *visited, size_t size)
+{
+    size_t dist_min = size * size;
+    size_t index_min = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (dist[i] < dist_min && !is_visited(visited, i))
+        {
+            dist_min = dist[i];
+            index_min = i;
+        }
     }
-  }
-  return 1;
+    return index_min;
 }
 
-gsl_spmatrix_uint *  init_visited(size_t num){
+size_t dijsktra(struct graph_t *graph, size_t src, size_t dst)
+{
+    size_t num = graph->num_vertices;
+    size_t visited[num];
+    size_t dist[num];
+    init_array_with_value(0, visited, num);
+    init_array_with_value(num+1, dist, num);
+    dist[src] = 0;
 
-  gsl_spmatrix_uint * visited = gsl_spmatrix_uint_alloc(1,num);
-  for (size_t i = 0 ; i<num ; i++){
-    gsl_spmatrix_uint_set(visited,0,i,0);
-  }
-  return visited;
-}
+    size_t index = 0;
+    size_t length = 0;
 
-gsl_spmatrix_uint * init_dist(size_t num){
-
-  gsl_spmatrix_uint * dist = gsl_spmatrix_uint_alloc(1,num);
-  for (size_t i = 0 ; i<num ; i++){
-    gsl_spmatrix_uint_set(dist,0,i,num+1);
-  }
-  return dist;
-}
-
-gsl_spmatrix_uint * init_prec(size_t num){
-
-  gsl_spmatrix_uint * prec = gsl_spmatrix_uint_alloc(1,num);
-  for (size_t i = 0 ; i<num ; i++){
-    gsl_spmatrix_uint_set(prec,0,i,i);
-  }
-  return prec;
-}
-
-int is_visited(gsl_spmatrix_uint * visited,size_t index){
-
-  return gsl_spmatrix_uint_get(visited,0,index) != 0;
-}
-
-size_t get_dist_min_not_visited(gsl_spmatrix_uint * dist,gsl_spmatrix_uint * visited){
-
-  size_t dist_min = dist->size2 * dist->size2;
-  size_t index_min = 0;
-  for (size_t i = 0 ; i <dist->size2 ; i++){
-    if (gsl_spmatrix_uint_get(dist,0,i) <= dist_min && (! is_visited(visited,i))){
-      dist_min = gsl_spmatrix_uint_get(dist,0,i);
-      index_min = i;
+    while (!all_visited(visited, num))
+    {
+        index = get_dist_min_not_visited(dist, visited, num);
+        visited[index] = 1;
+        for (size_t i = 0; i < num; i++)
+        {
+            if (is_connected(graph, index, i))
+            {
+                length = dist[index] + 1;
+                if (length < dist[i])
+                    dist[i] = length;
+            }
+        }
     }
-  }
-  return index_min;
+
+    return dist[dst];
 }
-  
-
-
-size_t dijsktra(struct graph_t * graph,size_t src,size_t dst){
-
-  size_t num = graph->num_vertices;
-  gsl_spmatrix_uint * visited = init_visited(num);
-  gsl_spmatrix_uint * dist = init_dist(num);
-  gsl_spmatrix_uint * prec = init_prec(num);
-  gsl_spmatrix_uint_set(dist,0,src,0);
-  size_t index = 0;
-  size_t length;
-  while (! all_visited(visited)){
-    index = get_dist_min_not_visited(dist,visited);
-    gsl_spmatrix_uint_set(visited,0,index,1);
-    /*
-    if (index == dst){
-      break;
-    }
-    */
-    for (size_t i = 0 ; i < num ; i++){
-      if (is_connected(graph,index,i)){
-	  length = gsl_spmatrix_uint_get(dist,0,index)+1;
-	  if (length < gsl_spmatrix_uint_get(dist,0,i)){
-	    gsl_spmatrix_uint_set(dist,0,i,length);
-	    gsl_spmatrix_uint_set(prec,0,i,index);
-	    
-	  }
-      }
-    }
-  }
-  gsl_spmatrix_uint_free(visited);
-  /*
-  size_t l = num + 1;
-  size_t vertice = dst;
-  size_t origin = dst;
-  //printf("\n dst = %ld \n",origin);
-  size_t compt = 0;
-  while ( origin != src && compt < num){
-
-    vertice = origin;
-    origin = gsl_spmatrix_uint_get(prec,0,origin);
-  }
- 
-  l = gsl_spmatrix_uint_get(dist,0,dst);
-  
-  
-  struct dijsktra_t * d = init_dijsktra(l,vertice);
-  return d;
-  */
-  length = gsl_spmatrix_uint_get(dist,0,dst);
-  gsl_spmatrix_uint_free(dist);
-  gsl_spmatrix_uint_free(prec);
-  return length;
-}
-
-/*
-size_t mini_dijsktra(struct graph_t * graph, size_t position, enum color_t id){
-
-  
-  size_t vertice;
-  size_t num =graph->num_vertices; 
-  size_t length = num+1;
-  //size_t choose;
-  for (size_t i = 0 ; i < num ; i++){
-    
-    if (is_owned(graph,get_next_player(id),i)){
-      //printf(" %ld ",i);
-      struct dijsktra_t * d = dijsktra(graph,position,i);
-
-      if (d->dist <= length){
-	length = d->dist;
-	vertice = d->prec;
-	//choose = i;
-      }
-      free_dijsktra(d);
-    }
-  }
-  return vertice;
-}
-*/
-
-
-
- 
-	
-	
-	    
-    
--- src/include/dijsktra.h
+++ src/include/dijsktra.h
@@ -3,17 +3,6 @@
 
 #include "graph.h"
 
-
-struct dijsktra_t;
-struct dijsktra_t * init_dijsktra(size_t dist, size_t prec);
-void free_dijsktra(struct dijsktra_t * d);
-int all_visited(gsl_spmatrix_uint * visited);
-gsl_spmatrix_uint *  init_visited(size_t num);
-gsl_spmatrix_uint * init_dist(size_t num);
-gsl_spmatrix_uint * init_prec(size_t num);
-int is_visited(gsl_spmatrix_uint * visited,size_t index);
-size_t get_dist_min_not_visited(gsl_spmatrix_uint * dist,gsl_spmatrix_uint * visited);
 size_t dijsktra(struct graph_t * graph,size_t src,size_t dst);
-//size_t mini_dijsktra(struct graph_t * graph, size_t position, enum color_t id);
 
 #endif // _DIJSKTRA_H_
